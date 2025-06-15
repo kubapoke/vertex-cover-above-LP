16c8043be7b2d66f4127e8d63819f66ab572d00d
kubapoke <q.paczka@gmail.com>
2025-06-16 01:14:31 +0200

    changed exp2 to check many test cases

--- vertex-cover-above-LP/testing/exp2.py
+++ vertex-cover-above-LP/testing/exp2.py
@@ -6,19 +6,25 @@ from vertex_cover import vertex_cover
 if __name__ == "__main__":
     num_vertices = 10  # 1000 # 10000 # 100000 # 1000000
     num_edges = 35
-    graph = generate_random_graph(num_vertices, num_edges)
+    repetitions = 1000
 
-    brute_solution = brute_solve(graph)
-    k = len(brute_solution)
+    for i in range(repetitions):
+        graph = generate_random_graph(num_vertices, num_edges, i)
 
-    false_result, false_cover = vertex_cover(graph, k - 1)
-    true_result, true_cover = vertex_cover(graph, k)
+        brute_solution = brute_solve(graph)
+        k = len(brute_solution)
 
-    if false_result == False and true_result == True and verify(graph, true_cover, k):
-        print("Experiment passed -- The vertex cover finds a solution for the found minimum k and does not for a lesser one.")
-    elif false_result == True:
-        print("Experiment failed -- Found a solution smaller than the smallest one.")
-    elif true_result == False:
-        print("Experiment failed -- Didn't find a solution of the found size.")
-    else:
-        print("Experiment failed -- The found solution is not correct.")
+        false_result, false_cover = vertex_cover(graph, k - 1)
+        true_result, true_cover = vertex_cover(graph, k)
+
+        if false_result == False and true_result == True and verify(graph, true_cover, k):
+            print("Experiment passed -- The vertex cover finds a solution for the found minimum k and does not for a lesser one.")
+        elif false_result == True:
+            print("Experiment failed -- Found a solution smaller than the smallest one.")
+            break
+        elif true_result == False:
+            print("Experiment failed -- Didn't find a solution of the found size.")
+            break
+        else:
+            print("Experiment failed -- The found solution is not correct.")
+            break
